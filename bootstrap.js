e351c90733c82a4118aca58be9bb8ab8d621b25a
tardis <14971857@qq.com>
2025-01-02 20:57:54 +0800

    feat: add pnpm support

--- bootstrap.js
+++ bootstrap.js
@@ -1,6 +1,7 @@
 const child_process = require('child_process');
-child_process.execSync('node -v', { stdio: 'inherit', cwd: __dirname });
-child_process.execSync('yarn -v', { stdio: 'inherit', cwd: __dirname });
+
+// 环境默认不含有 pnpm
+child_process.execSync('corepack enable', { stdio: 'inherit', cwd: __dirname });
 child_process.execSync('pnpm install', { stdio: 'inherit', cwd: __dirname });
 
 require('./index.js');
