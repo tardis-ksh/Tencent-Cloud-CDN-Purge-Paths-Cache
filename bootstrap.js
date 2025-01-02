6ab496a8070b9f41bf55e8bfbe0225471bb2c2bc
tardis <14971857@qq.com>
2025-01-02 20:52:31 +0800

    build: add bootstrap

--- bootstrap.js
+++ bootstrap.js
@@ -1,4 +1,6 @@
 const child_process = require('child_process');
+child_process.execSync('node -v', { stdio: 'inherit', cwd: __dirname });
+child_process.execSync('yarn -v', { stdio: 'inherit', cwd: __dirname });
 child_process.execSync('pnpm install', { stdio: 'inherit', cwd: __dirname });
 
 require('./index.js');
