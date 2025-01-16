acbf00471eca969b87639165ac8fd052f151a6d1
tardis <14971857@qq.com>
2025-01-16 15:06:25 +0800

    feat: update version

--- index.mjs
+++ index.mjs
@@ -1,16 +1,19 @@
 // actions core
-import core from '@actions/core';
-import { purgePathCache } from '@tardis-ksh/tencent/cdn'
+import core from "@actions/core";
+import { purgePathCache } from "@tardis-ksh/tencent/cdn";
 
 const inputs = {
-  secretId: core.getInput("secret_id"),
-  secretKey: core.getInput("secret_key"),
-  paths: core.getInput("paths")?.split(/\s+/).filter(Boolean),
-  flushType: core.getInput("flush_type"),
+  Paths: core.getInput("paths")?.split(/\s+/).filter(Boolean),
+  FlushType: core.getInput("flush_type"),
   urlEncode: core.getInput("url_encode") === "true",
   area: core.getInput("area"),
 };
 
+const credentials = {
+  secretId: core.getInput("secret_id"),
+  secretKey: core.getInput("secret_key"),
+};
+
 // console.log(inputs, "tencent cdn purge cache user inputs");
 
-purgePathCache(inputs)
+purgePathCache(credentials, inputs);
